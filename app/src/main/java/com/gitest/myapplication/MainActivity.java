6a5869ce2f9069c6eae7f3f7b242060f9cacc194
blueinline <djshanne@gmail.com>
2018-01-31 20:59:07 +0100

    release change last minute after closed (don't do it)

--- app/src/main/java/com/gitest/myapplication/MainActivity.java
+++ app/src/main/java/com/gitest/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_rename);
 
         // bug fixing release
+        //Release change last moment super important that 
     }
 }
